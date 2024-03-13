aaa09f99e00fd5265533c65515f1bc92cc97f6ac
Anastasiya <domnich@gmail.com>
2024-03-13 20:38:46 +0100

    part3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert('Hello');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    sctors: {},
+    genres: [],
+    privet: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?'),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
